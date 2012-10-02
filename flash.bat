becaec85c70f175c38a059833af89d61d4b3457e
lincomatic <lincomatic@hotmail.com>
2012-10-02 15:54:46 -0700

    add

---
+++ flash.bat
@@ -0,0 +1,5 @@
+avrdude -c usbtiny -p at90can128  -U lfuse:w:0xFF:m
+avrdude -c usbtiny -p at90can128  -U hfuse:w:0x19:m
+avrdude -c usbtiny -p at90can128  -U efuse:w:0xFF:m
+avrdude -c usbtiny -p at90can128 -U flash:w:LeafCANv2.hex
+
